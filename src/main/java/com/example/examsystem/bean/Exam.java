de469f7f095c99ee31972e32389a351fa168cc5c
sunzeping <2977898490@qq.com>
2020-03-22 23:48:48 +0800

    SZP EXAM

---
+++ src/main/java/com/example/examsystem/bean/Exam.java
@@ -0,0 +1,22 @@
+package com.example.examsystem.bean;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class Exam  implements Serializable
+{
+
+  private long eid;
+  private long tid;
+  private long sid;
+  private String ename;
+  private String estart;
+  private String eend;
+  private String eduration;
+  private String ecode;
+  private String eregtime;
+
+
+}
